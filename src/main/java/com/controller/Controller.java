327a6b1afa7678a896d3cb5d4ede70d557f7fe26
Renata Piazera <2023017441@aluno.restinga.ifrs.edu.br>
2024-02-04 02:31:33 -0300

    feat: input check

--- src/main/java/com/controller/Controller.java
+++ src/main/java/com/controller/Controller.java
@@ -3,6 +3,28 @@ import main.java.com.product.handler.ProductHandler;
 
 public class Controller {
 
+    public void checkOption(int option) {
+        switch(option) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+            case 5:
+                System.out.println("5");
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+    }
+
     public void menu() {
         System.out.println("=== MENU ===");
         System.out.println("1 - List products");
