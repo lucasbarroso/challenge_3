d4d749f0ecb4a997bf76e9fb74579c0268d9ad18
lucasbarroso <lucas.barrooso@gmail.com>
2024-02-05 05:14:07 -0300

    fix:adding some building dependencies to fix pom.xml and testing

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import com.example.repository.Repository;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Repository conn= new Repository();
+        conn.getConnection();
+        
     }
 }
--- src/main/java/com/example/repository/Repository.java
+++ src/main/java/com/example/repository/Repository.java
@@ -1,20 +1,21 @@
 package com.example.repository;
 import java.sql.*;
 
-public abstract class Repository {
-    private static String host =System.getenv("TESTEPHP_POSTGRES_HOST");
-    private static final String URL = "jdbc:postgresql://"+ host+ ":5432/postgres"; //
+public class Repository {
+    private static String host ="";
+    private static final String URL = "jdbc:postgresql://"+ host+ ":5432/postgres"; 
     private static final String USERNAME = "";
-    private static final String PASSWORD = System.getenv("TESTEPHP_POSTGRES_PASSWORD");;
+    private static final String PASSWORD = "";
     private static Connection con;
     
-    protected Connection getConnection() {
+    public Connection getConnection() {
         try {
             
             if(con == null || con.isClosed()){
                 con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
             }
+            System.out.println("deu certo?");
             return con;
             
 
