952bbb3685149100823f13dae3373e9973a9e3ea
lucasbarroso <lucas.barrooso@gmail.com>
2024-02-06 17:19:47 -0300

    last commit before merge

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,15 +2,14 @@ package com.example;
 
 import com.example.repository.Product;
 import com.example.repository.ProductRepository;
-import com.example.repository.Repository;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         ProductRepository productOperation = new ProductRepository();
         Product productD = new Product("product D", "New cat toy trinket", 99.00);
-        
-        // productOperation.updateById(3, productD);
+
         // productOperation.save(productD);
         
         // System.out.println(productOperation.findAll());
